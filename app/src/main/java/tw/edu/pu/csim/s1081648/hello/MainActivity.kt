8ad80c0f342916b325bb788570f4951e6e5e39f3
youci <s1081648@pu.edu.tw>
2021-03-12 14:39:36 +0800

    1

--- app/src/main/java/tw/edu/pu/csim/s1081648/hello/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1081648/hello/MainActivity.kt
@@ -2,10 +2,19 @@ package tw.edu.pu.csim.s1081648.hello
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        TestMaster()
     }
+
+    fun TestMaster(){
+        var txv: TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "分支"
+        txv.text = "dev分支"
+    }
+
 }
