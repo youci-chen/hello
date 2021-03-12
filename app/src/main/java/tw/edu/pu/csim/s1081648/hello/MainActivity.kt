3fec54dbf0e67281e0f74adbf224556b1b83d68a
youci <s1081648@pu.edu.tw>
2021-03-12 13:51:44 +0800

    1

---
+++ app/src/main/java/tw/edu/pu/csim/s1081648/hello/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.edu.pu.csim.s1081648.hello
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
